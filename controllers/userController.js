464ea2f6403545e835b403ac66da91e7682c935d
firas1996 <firas.hm.1996@gmail.com>
2025-10-20 15:06:30 +0100

    aza

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,3 +36,53 @@ exports.getAllUsers = async (req, res) => {
     });
   }
 };
+
+// Get User By Id
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: user,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failll",
+      message: error,
+    });
+  }
+};
+
+exports.updateUserById = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: user,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failll",
+      message: error,
+    });
+  }
+};
+
+exports.deleteUserById = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failll",
+      message: error,
+    });
+  }
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { createUser, getAllUsers } = require("../controllers/userController");
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+} = require("../controllers/userController");
 
 const Router = express.Router();
 
 Router.post("/createuser", createUser);
 Router.get("/users", getAllUsers);
+Router.get("/user/:id", getUserById);
 
 module.exports = Router;
