8b17a71a9134f65576ec94cdc07ca9cbffbf65d3
firas1996 <firas.hm.1996@gmail.com>
2025-10-20 14:16:10 +0100

    aa

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,32 @@
 const User = require("../models/userModel");
 
+// Create a new user
 exports.createUser = async (req, res) => {
   try {
-    console.log("aa", req.body);
     const newUser = await User.create(req.body);
     res.status(201).json({
       status: "success",
       data: {
         user: newUser,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failll",
+      message: error,
+    });
+  }
+};
+
+// get all users
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        users: users,
       },
     });
   } catch (error) {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, getAllUsers } = require("../controllers/userController");
 
 const Router = express.Router();
 
 Router.post("/createuser", createUser);
+Router.get("/users", getAllUsers);
 
 module.exports = Router;
